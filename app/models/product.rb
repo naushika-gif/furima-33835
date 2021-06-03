6417253294f691e07c1056cc3fe935d926702387
naushika-gif <nashika.kudo@gmail.com>
2021-06-03 16:00:56 +0900

    バリデーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,26 @@ class Product < ApplicationRecord
   belongs_to :delivery_cost_id
   belongs_to :delivery_from_id
   belongs_to :delivery_date_id
+
+  with_options presence:true do
+    validates :name
+    validates :explanation
+    validates :category_id
+    validates :condition_id
+    validates :delivery_cost_id
+    validates :delivery_from_id
+    validates :delivery_date_id
+
+  with_options numericality: { other_than: 1} do
+      validates :category_id
+      validates :condition_id
+      validates :delivery_cost_id
+      validates :delivery_from_id
+      validates :delivery_date_id
+  end
+
+   with_options numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"} do
+      validates :price  
+   end
+  end
 end
