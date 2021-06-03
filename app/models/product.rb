f232e00e2e42fd09bcf56d3c85240e4bcc7d13a3
naushika-gif <nashika.kudo@gmail.com>
2021-06-03 15:33:21 +0900

    ActiveHashの導入
    -category_id
    -condition_id
    -delivery_cost_id
    -delivery_from_id
    -delivery_date_id

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,10 @@
 class Product < ApplicationRecord
-  belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id
+  belongs_to :condition_id
+  belongs_to :delivery_cost_id
+  belongs_to :delivery_from_id
+  belongs_to :delivery_date_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_040229) do
+ActiveRecord::Schema.define(version: 2021_06_03_044920) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
