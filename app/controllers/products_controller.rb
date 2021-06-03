b3badd430d21aa85894f51f4bf211344f14ff60c
naushika-gif <nashika.kudo@gmail.com>
2021-06-03 13:10:54 +0900

    ActiveStorageの導入②
    画像を保存する機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,8 @@ class ProductsController < ApplicationController
     
   end
 
+  private 
+  
+  def product_params
+    params.require(:product).permit(:content, :image).merge(user_id: current_user.id)
 end
