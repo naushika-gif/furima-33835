fd774860294e27ee42d7f2ee19b4852ac596444f
naushika-gif <nashika.kudo@gmail.com>
2021-06-03 19:25:32 +0900

    モデル単体テストコード(途中)

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :product do
-    
+    name        {Faker::Lorem.sentence}
+    explanation {Faker::Lorem.sentence}
+    image {Faker::Lorem.sentence}
+    category_id {2}
+    condition_id {2}
+    delivery_cost_id {2}
+    delivery_from_id {2}
+    delivery_date_id {2}
+    price {'5000'}
+    association :user
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
-
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @product = FactoryBot.build(:product)
+  end
+
+  describe '出品する商品の保存' do
+    context "出品する商品のデータが保存できるとき" do
+      it '全ての項目の入力が存在されていれば登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+    context '出品する商品のデータが保存できないとき'
+    it 'imageが添付されていないと登録できない' do
+      @product.image = ''
+      @product.valid?
+      binding.pry
+    end
+    it 'nameが空では登録できない' do
+    end
+    it 'explanationが空では登録できない' do
+    end
+    it 'category_idが選択されていないと登録できない' do
+    end
+    it 'condition_idが選択されていないと登録できない' do
+    end
+    it 'delivery_cost_idが選択されていないと登録できない' do
+    end
+    it 'delivery_from_idが選択されていないと登録できない' do
+    end
+    it 'delivery_date_idが選択されていないと登録できない' do
+    end
+    it 'priceが空だと登録できない' do
+    end
+    it '' do
+    end
+    it '' do
+    end
+    it '' do
+    end
+  end
 end
