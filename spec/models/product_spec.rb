b35704a8b0ca971dfc7f3743822640c3a9d1d48c
naushika-gif <nashika.kudo@gmail.com>
2021-06-04 16:48:10 +0900

    単体テストコード

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -10,33 +10,58 @@ RSpec.describe Product, type: :model do
         expect(@product).to be_valid
       end
     end
-    context '出品する商品のデータが保存できないとき'
-    it 'imageが添付されていないと登録できない' do
-      @product.image = ''
+    context '出品する商品のデータが保存できないとき' do
+     it 'imageが添付されていないと登録できない' do
+      if @product.image.attached?
+      else
       @product.valid?
-      binding.pry
-    end
-    it 'nameが空では登録できない' do
-    end
-    it 'explanationが空では登録できない' do
+      end
+     end
+     it 'nameが空では登録できない' do
+      @product.name = ''
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+     end
+     it 'explanationが空では登録できない' do
+      @product.explanation = ''
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Explanation can't be blank")
     end
     it 'category_idが選択されていないと登録できない' do
+      @product.category_id = '1'
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Category must be other than 1")
     end
     it 'condition_idが選択されていないと登録できない' do
+      @product.condition_id = '1'
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Condition must be other than 1")
     end
     it 'delivery_cost_idが選択されていないと登録できない' do
+      @product.delivery_cost_id = '1'
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Delivery cost must be other than 1")
     end
     it 'delivery_from_idが選択されていないと登録できない' do
+      @product.delivery_from_id = '1'
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Delivery from must be other than 1")
     end
     it 'delivery_date_idが選択されていないと登録できない' do
+      @product.delivery_date_id = '1'
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Delivery date must be other than 1")
     end
     it 'priceが空だと登録できない' do
+      @product.price = ''
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Price can't be blank","Price is invalid")
     end
-    it '' do
-    end
-    it '' do
-    end
-    it '' do
+    it 'priceは全角数字だと登録できない' do
+      @product.price = '００００'
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Price is invalid")
+     end
     end
   end
 end
